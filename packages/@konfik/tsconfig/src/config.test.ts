249cdbf670315929810327ebd94ef68f542aa5fe
harrysolovay <harrysolovay@gmail.com>
2022-01-23 11:38:50 -0500

    fix conflicts

--- packages/@konfik/tsconfig/src/config.test.ts
+++ packages/@konfik/tsconfig/src/config.test.ts
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { _ } from "../../../konfik";
-=======
-=======
->>>>>>> 8da8708 (fix test exclusion from tsc)
-// TODO: use correct yarn 2 convention to resolve
-<<<<<<< HEAD
-import { _ } from "../../../konfik/src/lib";
->>>>>>> bd2a928 (merge main and resolve conflicts)
-import { Ts } from "./config";
+import { _ } from '../../../konfik'
+import { Ts } from './config'
 
 const MyTs = Ts({
   compilerOptions: {
@@ -16,88 +7,24 @@ const MyTs = Ts({
     allowJs: false,
     composite: true,
   },
-  exclude: ["node_modules"],
-  include: _,
-<<<<<<< HEAD
-  extends: _,
-  watchOptions: _,
-});
-
-const Almost = MyTs({
-  // TODO: retain symbol-bound docs.
-  include: ["."],
-  extends: _,
-  watchOptions: _,
-});
-
-const SoClose = Almost({
-  extends: "",
-  watchOptions: _,
-});
-
-const final = SoClose({
-  watchOptions: {},
-=======
-});
-
-const final = MyTs({
-  // TODO: retain symbol-bound docs.
-  include: ["YO"],
->>>>>>> bd2a928 (merge main and resolve conflicts)
-});
-=======
-// import { _ } from '../../../konfik/src/lib'
-// import { Ts } from './config'
-
-// const MyTs = Ts({
-//   compilerOptions: {
-//     declaration: true,
-//     allowJs: false,
-//     composite: true,
-//   },
-//   exclude: ['node_modules'],
-//   include: _,
-// })
-
-// const final = MyTs({
-//   // TODO: retain symbol-bound docs.
-//   include: ['YO'],
-// })
-
-export {}
-<<<<<<< HEAD
->>>>>>> bcff76c (move files into core and set up ava)
-=======
-=======
-import { _ } from "../../../konfik";
-import { Ts } from "./config";
-
-const MyTs = Ts({
-  compilerOptions: {
-    declaration: true,
-    allowJs: false,
-    composite: true,
-  },
-  exclude: ["node_modules"],
+  exclude: ['node_modules'],
   include: _,
   extends: _,
   watchOptions: _,
-});
+})
 
 const Almost = MyTs({
   // TODO: retain symbol-bound docs.
-  include: ["."],
+  include: ['.'],
   extends: _,
   watchOptions: _,
-});
+})
 
 const SoClose = Almost({
-  extends: "",
+  extends: '',
   watchOptions: _,
-});
+})
 
 const final = SoClose({
   watchOptions: {},
-});
->>>>>>> 9bc4c39 (fix test exclusion from tsc)
->>>>>>> 8da8708 (fix test exclusion from tsc)
+})
