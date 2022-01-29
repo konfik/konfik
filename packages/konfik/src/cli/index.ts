77b7906457b3ed892a31aec78212c24cfdd9dfd9
Maxwell Brown <maxwellbrown1990@gmail.com>
2022-01-29 17:39:15 +0000

    chore(konfik): initial work on esbuild for cli

--- packages/konfik/src/cli/index.ts
+++ packages/konfik/src/cli/index.ts
@@ -1,24 +1,3 @@
-import { program } from 'commander'
-// import * as fs from 'node:fs/promises'
-import * as path from 'path'
-import { fileURLToPath } from 'url'
+import { run } from "@konfik/cli/cli"
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
-
-export const run = async () => {
-  // TODO: get version
-  const version = ''
-  program
-    .version(version)
-    .option('-c, --config <string>', 'TODO: description', 'project')
-    .action(async ({ config: configPath }: { config: string }) => {
-      const fullConfigPath = path.relative(__dirname, configPath)
-      // const fullConfigPath = configPath
-
-      console.log({ fullConfigPath, __dirname })
-
-      // const register  = await import('@swc/register')
-      await import(fullConfigPath)
-    })
-    .parse(process.argv)
-}
+run()
