c2d44bcd2fc9a832866ee6950d8668b9509162eb
Maxwell Brown <maxwellbrown1990@gmail.com>
2022-01-29 15:15:48 +0000

    fix(konfik): fix link.mjs

--- link.mjs
+++ link.mjs
@@ -8,6 +8,6 @@ for (const example of examples) {
   const virtualKonfikRoot = `examples/${example}/node_modules/.konfik/github.com/konfik/konfik`
   await $`mkdir -p ./${virtualKonfikRoot}`
 
-  await $`rm $PWD/${virtualKonfikRoot}/plugins`
+  await $`rm -rf $PWD/${virtualKonfikRoot}/plugins`
   await $`ln -sf $PWD/plugins $PWD/${virtualKonfikRoot}/plugins`
 }
