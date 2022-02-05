a03e473130943aeea489d8ec004efbca9a865cc7
harrysolovay <harrysolovay@gmail.com>
2022-02-05 10:33:56 -0500

    use globalThis instead of standalone map

--- packages/@konfik/core/src/konfik-factory.ts
+++ packages/@konfik/core/src/konfik-factory.ts
@@ -5,7 +5,11 @@ export interface KonfikFactoryProps<Brand extends PropertyKey, Blueprint> {
 }
 type AnyKonfikFactoryProps = KonfikFactoryProps<PropertyKey, any>
 // TODO: is this internally-managed state sinful?
-const factoryPropsLookup = new Map<any, AnyKonfikFactoryProps>()
+const FactoryPropsLookupKey = Symbol.for('@konfik/core.konfik-factory.FactoryPropsLookupKey')
+
+const globalTh = globalThis as any
+
+globalTh[FactoryPropsLookupKey] = new Map<any, AnyKonfikFactoryProps>()
 
 // TODO: allow additional constraints to be specified / produce generic factory.
 // This would all ow us to support VSCode extension types.
@@ -13,7 +17,7 @@ const factoryPropsLookup = new Map<any, AnyKonfikFactoryProps>()
 export const KonfikFactory = <Blueprint>() => {
   return <Brand extends PropertyKey>(factoryProps: KonfikFactoryProps<Brand, Blueprint>) => {
     return <Supplied extends Blueprint>(config: Supplied) => {
-      factoryPropsLookup.set(config, factoryProps)
+      globalTh[FactoryPropsLookupKey].set(config, factoryProps)
       // This way, we can spread configs into other configs.
       return config
     }
@@ -21,7 +25,7 @@ export const KonfikFactory = <Blueprint>() => {
 }
 
 export const getFactoryConfig = (supplied: unknown): AnyKonfikFactoryProps => {
-  const factoryProps = factoryPropsLookup.get(supplied)
+  const factoryProps = globalTh[FactoryPropsLookupKey].get(supplied)
   if (!factoryProps) {
     throw new Error('TODO: throw this the effect-y way.')
   }
