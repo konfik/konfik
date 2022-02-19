e46529f5d420e4d8de88d978ae40803c2bd81273
Johannes Schickling <schickling.j@gmail.com>
2022-02-19 18:09:16 +0100

    package-json plugin: Sort dependencies, devDependencies and peerDependency entries alphabetically closes #32

--- plugins/package-json/src/index.ts
+++ plugins/package-json/src/index.ts
@@ -6,8 +6,32 @@ export type PackageJsonKonfikBrand = typeof PackageJsonKonfikBrand
 
 export const PackageJsonKonfik = KonfikFactory<PackageJsonRaw>()({
   brand: PackageJsonKonfikBrand,
-  toString: (config) => JSON.stringify(config),
+  toString: (config) => {
+    if (config.dependencies) {
+      config.dependencies = sortObjectAlphabetically(config.dependencies)
+    }
+
+    if (config.devDependencies) {
+      config.devDependencies = sortObjectAlphabetically(config.devDependencies)
+    }
+
+    if (config.peerDependencies) {
+      config.peerDependencies = sortObjectAlphabetically(config.peerDependencies)
+    }
+
+    return JSON.stringify(config)
+  },
   // NOTE we can't simply use `json` here since prettier seems to format `package.json` files with the `json-stringify` parser by default
   // We should come up with a better solution to this
   fileType: 'json-stringify',
 })
+
+const sortObjectAlphabetically = <T extends Record<string, any>>(obj: T) => {
+  const sorted = {} as T
+  Object.keys(obj)
+    .sort()
+    .forEach((key: keyof T) => {
+      sorted[key] = obj[key]
+    })
+  return sorted
+}
